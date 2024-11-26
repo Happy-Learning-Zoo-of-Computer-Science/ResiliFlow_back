7ee60a43a5f9c218d593034d34f1c9d736e56cab
ChangYu Huang <jack.mainwork@gmail.com>
2024-11-26 18:38:08 -0500

    Test get configurations API and rename testing file to fit unittest rules.

--- app/routes/project_serialization.py
+++ app/routes/project_serialization.py
@@ -21,6 +21,73 @@ def get_supported_languages() -> Response:
     return jsonify(ProjectSerializor.get_supported_langauges())
 
 
+@bp.route("/frameworks", methods=["GET"])
+def get_supported_frameworks() -> Response:
+    """Get supported frameworks of a language.
+
+    Returns:
+        Response: [frameworks].
+    """
+
+    payload = request.get_json()
+    if "language" not in payload:
+        error = 'Missing field "language".'
+        logging.error(error)
+        return jsonify({"error": error}), 400
+    result = ProjectSerializor.get_supported_frameworks(payload["language"])
+    return jsonify(result)
+
+
+@bp.route("/configurations/supported", methods=["GET"])
+def get_supported_configurations() -> Response:
+    """Get supported configurations of a language and a framework.
+
+    Returns:
+        Response: {configuration: attributes}, 400 (Missing field), 415 (No payload)
+    """
+
+    payload = request.get_json()
+    if "language" not in payload:
+        error = 'Missing field "language".'
+        logging.error(error)
+        return jsonify({"error": error}), 400
+
+    language = payload["language"]
+    framework = payload.get("framework", None)
+    result = ProjectSerializor.get_configurations(language, framework)
+    return jsonify(result)
+
+
+@bp.route("/configurations/initialized", methods=["GET"])
+def get_initialized_configurations() -> Response:
+    """Get initialized configurations of a language and a framework in a path.
+
+    Returns:
+        Response: [configuration files name], 400 (Missing field, path doesn't exist), 415 (No payload)
+    """
+
+    payload = request.get_json()
+    if "language" not in payload:
+        error = 'Missing field "language".'
+        logging.error(error)
+        return jsonify({"error": error}), 400
+    if "path" not in payload:
+        error = 'Missing field "path".'
+        logging.error(error)
+        return jsonify({"error": error}), 400
+
+    path = payload["path"]
+    language = payload["language"]
+    framework = payload.get("framework", None)
+    try:
+        result = ProjectSerializor.get_initialized_configurations(
+            path, language, framework
+        )
+        return jsonify(result)
+    except NotADirectoryError as ex:
+        return jsonify({"error": ex.args[0]}), 400
+
+
 @bp.route("/validate", methods=["GET"])
 def is_project_initialized() -> Response:
     """Check is a project initialized.
