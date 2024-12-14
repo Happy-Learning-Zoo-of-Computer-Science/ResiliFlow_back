f83f9ccfd4f446f4ca8ac5c64cab1abdf5009c6b
zjkung1123 <zjkung1123@gmail.com>
2024-12-14 02:42:50 -0500

    Implement asynchronous pipeline execution using threading

--- app/services/pipelines/pipeline_execute_service.py
+++ app/services/pipelines/pipeline_execute_service.py
@@ -1,5 +1,6 @@
 import subprocess
 import uuid
+import threading
 
 
 class PipelineExecuteService:
@@ -7,6 +8,8 @@ class PipelineExecuteService:
         self.exec_result = dict[str, str]()
 
     def get_execution_status(self, id: str) -> str:
+        if id not in self.exec_result:
+            return "NOT FOUND"
         return self.exec_result[id]
 
     def execute_pipeline(self, pipeline_name: str, yaml_path: str) -> str:
@@ -15,24 +18,24 @@ class PipelineExecuteService:
         output_file = f"./pipeline-{id}.log"
 
         # Open the file in write mode
-        with open(output_file, "w", encoding="utf-8") as file:
-            file.write(
-                f"Executing pipeline {pipeline_name} with yaml path {yaml_path}\n"
-            )
-            # # Run the subprocess command
-            process = subprocess.Popen(
-                [
-                    "./act",
-                    "-W",
-                    ".github/workflows/test-on-pr.yml",
-                    "--container-architecture",
-                    "linux/amd64",
-                ],
-                stdout=file,  # Redirect stdout to the file
-                stderr=file,  # Redirect stderr to the file
-                text=True,  # Ensure output is written as text, not bytes
-            )
-            process.wait()
-            self.exec_result[id] = "DONE"
+        def run_subprocess():
+            with open(output_file, "w", encoding="utf-8") as file:
+                process = subprocess.Popen(
+                    [
+                        "./act",
+                        "-W",
+                        ".github/workflows/test-on-pr.yml",
+                        "--container-architecture",
+                        "linux/amd64",
+                    ],
+                    stdout=file,  # Redirect stdout to the file
+                    stderr=file,  # Redirect stderr to the file
+                    text=True,  # Ensure output is written as text, not bytes
+                )
+                process.wait()
+                self.exec_result[id] = "DONE"
+
+        thread = threading.Thread(target=run_subprocess)
+        thread.start()
 
         return id
